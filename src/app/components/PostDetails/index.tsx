a3d3062f1998ad6adf80ebd1e207b237e9cc5381
Luciano Machado <lucianoo_machado@hotmail.com>
2025-11-23 11:06:08 -0300

    feat: add component Post Details

---
+++ src/app/components/PostDetails/index.tsx
@@ -0,0 +1,21 @@
+import styles from './postDetails.module.css';
+import Date from '../Date';
+
+export type PostDetailsProps = {
+  date: string;
+  author: string;
+  category: string;
+};
+
+export default function PostDetails({
+  date,
+  author,
+  category,
+}: PostDetailsProps) {
+  return (
+    <div className={styles.postDetails}>
+      Publicado em <Date date={date} />
+      por {author} | {category}
+    </div>
+  );
+}
--- src/app/containers/Post/index.tsx
+++ src/app/containers/Post/index.tsx
@@ -3,6 +3,7 @@ import Header from '@/app/components/Header';
 import { Heading } from '@/app/components/Heading';
 import { MainContainer } from '@/app/components/MainContainer';
 import { PostCover } from '@/app/components/PostCover';
+import PostDetails from '@/app/components/PostDetails';
 import { PostData } from '@/app/domain/posts/post';
 
 export type PostProps = {
@@ -16,6 +17,11 @@ export const Post = ({ post }: PostProps) => {
       <MainContainer>
         <Heading>{post.title}</Heading>
         <PostCover coverUrl={post.cover.url} alt={post.title} />
+        <PostDetails
+          author={post.author.name}
+          category={post.category.name}
+          date={post.createdAt}
+        />
         <article dangerouslySetInnerHTML={{ __html: post.content }} />
       </MainContainer>
       <Footer />
