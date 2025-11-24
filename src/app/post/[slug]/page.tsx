ae17c35a2b17f132dde01b3ca20300f92b9354cf
Luciano Machado <lucianoo_machado@hotmail.com>
2025-11-24 08:54:06 -0300

    feat: add Head to containers

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,46 @@
 import { Post } from '@/app/containers/Post';
 import { getPostBySlug } from '@/app/data/posts/get-post';
+import { removeHtml } from '@/app/utils/remove-html';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 type Props = {
   params: Promise<{ slug: string }>;
 };
+
+type MetaDataProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: MetaDataProps): Promise<Metadata> {
+  const resolvedParams = await params;
+  const post = await getPostBySlug(resolvedParams.slug);
+
+  if (!post) {
+    return { title: 'Post não encontrado' };
+  }
+
+  const cleanDescription = removeHtml(post.content).slice(0, 150) + '...';
+
+  return {
+    title: post.title,
+    description: cleanDescription, // Agora o Google recebe texto puro!
+    openGraph: {
+      title: post.title,
+      description: cleanDescription, // O WhatsApp também agradece
+      images: [
+        {
+          url: post.cover?.url || '',
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+  };
+}
 
 export default async function PostPage(props: Props) {
   const params = await props.params;
